31baea55e51a8d010d1f453aaf95d7026f15d02e
abhinav-sinhaa <76272472+abhinav-sinhaa@users.noreply.github.com>
2021-06-26 08:49:16 +0530

    Find section connection with backend part and database to dynamically load the data of the pincode entered by the user

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,40 @@
 const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const pool = new Pool ({
+    user : 'postgres',
+    host : 'localhost',
+    database : 'vaccine_hack21',
+    password : 'test',
+    port : 5432    
+})
+
+app.use(cors());
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+)
+  
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    let pincode = request.query.pincode;
+    let query = 'SELECT * FROM ' + `"${pincode}"` + ';'
+    console.log(query);
+
+    pool.query(query, (err,res) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(res);
+            response.send(res.rows);
+        }
+    })
+})
+
+app.listen(8080, () => {
+    console.log('server working');
+})
--- frontend/src/components/available.js
+++ frontend/src/components/available.js
@@ -2,6 +2,32 @@ import React from 'react';
 import '../styles/available.css';
 
 class Availabile extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            pincode : ''
+        }
+    }
+
+    onChange = (event) => {
+        this.setState({[event.target.id]: event.target.value})
+    }
+
+    onSubmit = (event) => {
+        console.log(this.state.pincode);
+        event.preventDefault();
+
+        let requestOptions = {
+            method: 'GET'
+        };
+
+        fetch(`http://localhost:8080/?pincode=${this.state.pincode}`,requestOptions)
+            .then(response => response.json())
+            .then(console.log)
+            .catch(console.log)
+    }
+
+
     render() {
         return(
             <div className = 'container find'>
@@ -11,12 +37,12 @@ class Availabile extends React.Component {
                         <p id = 'enterpin'>
                             Enter your pincode to find mobile vans in different localities near you.
                         </p>
-                        <form className='row'>
+                        <form className='row' onSubmit={this.onSubmit}>
                         <div className="col-md-9 col-lg-9 col-xl-9 col-sm-9 col-9">
-                            <input type="text" class="form-control" id="pincode"/>
+                            <input type="text" class="form-control" id="pincode" value={this.state.pincode} onChange={this.onChange}/>
                         </div>
                         <div className = "col-md-3 col-lg-3 col-xl-3 col-sm-3 col-3">
-                            <button className="btn btn-dark findBtn">Find</button>
+                            <button type='submit' className="btn btn-dark findBtn">Find</button>
                         </div>
                         </form>
                     </div>
